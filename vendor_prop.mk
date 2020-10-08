35b9defb36a8616b1b76a7f5e86bd6344a4ba530
Quallenauge <Hamsi2k@freenet.de>
2020-10-08 22:32:01 +0200

    bpf: Force enable bpf.
    Some variants of this target were launched with Android L (kernel 3.10).
    This causes the BpfUtils to not use bpf.
    Since we upgraded to kernel 4.9, this detection doesn't work,
    but on R there's an switch, set via property ro.kernel.ebpf.supported
    to force enable 4.9 bpf mode.
    
    Change-Id: I263e3aaa5a9a63cd027d8750c7cad0f34f432ed2

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,6 +2,10 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.media.avsync=true
 
+# Bpf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.ebpf.supported=1
+
 # USB configfs
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.sys.usb.udc=700d0000.xudc \
