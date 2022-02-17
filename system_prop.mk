541f573ac5e2ccaa9c408b62d1317ab9bcf6e52b
Nolen Johnson <johnsonnolen@gmail.com>
2022-02-17 15:47:40 -0500

    properties: Split per partition
    * `PRODUCT_PROPERTY_OVERRIDES` now tosses things on
      `/vendor` by default. We need to dictate where they
      should end up.
    
    Change-Id: I4fa9639f127ec54d5e7d015db5b804815b43dfde

--- system_prop.mk
+++ system_prop.mk
@@ -1,15 +1,6 @@
-# AV
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.media.avsync=true
-
 # Gamestreaming specific properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.gamestream.display.optimize=1
-
-# USB configfs
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.sys.usb.udc=700d0000.xudc \
-    sys.usb.controller=700d0000.xudc
 
 # Force sw compositing
 PRODUCT_PROPERTY_OVERRIDES += \
