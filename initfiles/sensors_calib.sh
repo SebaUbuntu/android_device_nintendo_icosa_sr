51ce6775242be204b56fcba0888bb316d2357016
Thomas Makin <halorocker89@gmail.com>
2022-03-20 04:38:34 +0000

    sensors: init: add sensor calibration script
    - Add sensor calibration init service and script
    - Update sensor defconfig to support "factory" calibration

---
+++ initfiles/sensors_calib.sh
@@ -0,0 +1,10 @@
+#!/system/xbin/bash
+ST_CALIBRATION_PERSIST_DIR="${1}"
+ST_FACTORY_DATA_PATH="${2}"
+
+echo "Checking gyroscope calibration..."
+cmp --silent "${ST_FACTORY_DATA_PATH}/gyro.txt" "${ST_CALIBRATION_PERSIST_DIR}/gyro.txt" || \
+    (echo "Changes found! Copying now..." && \
+    cp -f ${ST_CALIBRATION_PERSIST_DIR}/gyro.txt ${ST_FACTORY_DATA_PATH}/gyro.txt)
+
+echo "Calibration table up to date."
