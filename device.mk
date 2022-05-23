08717e62e6a42ee3fed6ee4d5830de219c94d4fc
Thomas Makin <halorocker89@gmail.com>
2022-05-22 21:10:22 -0400

    TEMP: nuke sensors

--- device.mk
+++ device.mk
@@ -122,13 +122,13 @@ PRODUCT_PACKAGES += \
 endif
 
 # STMicroElectronics IMU
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     android.hardware.sensors@1.0-impl \
     sensors.stmicro \
     sensors_calib
 
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.light.xml
